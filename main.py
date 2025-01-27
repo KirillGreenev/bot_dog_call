8c1fa2aaf0d79a8775e883aa6e94f60a982b7985
kirill <crownbarrel@gmail.com>
2025-01-27 16:13:03 +0500

    added prediction handler

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
+import random
 import requests
 import telebot
+from telebot import types
 from config import BOT_TOKEN
 from parser_group import get_members_group
 from multiprocessing import Process, Queue
+from repository import create_connection,create_table, select_all_prediction, close_connection
+
+sql_create_prediction_table = """
+        CREATE TABLE IF NOT EXISTS prediction (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            text TEXT NOT NULL
+        );
+        """
 
 bot = telebot.TeleBot(BOT_TOKEN)
+conn = create_connection("mydatabase.db")
+create_table(conn, sql_create_prediction_table)
+
 
 
 @bot.message_handler(commands=['start'])
@@ -26,7 +39,7 @@ def dog_call(message):
                      f"Собачий созыв!! АУУУУУУ🐺🌙\n{members_group}\nпоставьте реакцию дизлайка, если вы не придете в созвон, это важно")
 
 
-@bot.message_handler(commands=['котэ'])
+@bot.message_handler(commands=['котэ', 'cat'])
 def get_cat(message):
     respons = requests.get('https://api.thecatapi.com/v1/images/search')
     if respons.status_code == 200:
@@ -34,9 +47,24 @@ def get_cat(message):
     else:
         bot.send_message(message.chat.id, 'Здесь должна была быть картинка с котиком :(')
 
+@bot.message_handler(commands=['pr','судьба', 'предсказание'])
+def get_prediction(message):
+    if conn is not None:
+        r = select_all_prediction(conn)
+        if len(r) == 0:
+            bot.send_message(message.chat.id, "В данный момент предсказаний нету, но мы их обязательно добавим")
+        else:
+            ms = random.choice(r)
+            name = '@' + message.from_user.username if message.from_user.username else message.from_user.first_name
+            bot.send_message(message.chat.id, f"🔮")
+            bot.send_message(message.chat.id, f"Предсказание для {name}:\n{ms}")
+    else:
+        bot.send_message(message.chat.id, "Упс что-то пошло не так, но мы это обязательно исправим")
+
 
 if __name__ == '__main__':
     try:
         bot.polling(none_stop=True)
+        close_connection(conn)
     except Exception as e:
         print(f"Ошибка в боте: {e}")
